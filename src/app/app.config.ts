ede0e10c3afb86345482a1a91fd5ac7915b6a008
Imran Ali <152004405+imranali2112@users.noreply.github.com>
2025-04-14 17:06:47 +0500

    multi language app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 
+// Factory for translation loader
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+// Application config
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    // provideRouter(routes),
+
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient]
+        }, 
+        defaultLanguage: 'english',
+      })
+    ),
+    provideHttpClient(withInterceptorsFromDi())
+  ]
 };
---
+++ src/app/components/translate/translate.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-translate',
+  imports: [TranslateModule],
+  templateUrl: './translate.component.html',
+  styleUrl: './translate.component.scss'
+})
+export class TranslateComponent {
+  translate: TranslateService = inject(TranslateService);
+
+  ngOnInit(): void{
+     
+    this.translate.use('en');
+  }
+
+  onEnglish(lang: string) {
+    this.translate.use(lang);
+  }
+
+  onUrdu(lang: string) {
+    this.translate.use(lang);
+  }
+
+}
